83e716c7471d9ae19ebb363bd140099ddd0920a4
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-07-15 10:04:23 -0300

    Update exercicio6.py

--- exercicio6.py
+++ exercicio6.py
@@ -1,9 +1,21 @@
-print('Lembre-se de usar "." em vez de "," para as casas decimais dos números!!!\n')
+"""
+Exercício 6
+Crie um programa que peça ao usuário três números (pode ser decimal ou inteiro) e calcule a média desses números.
+O programa deve exibir a média com duas casas decimais.
+Salve como "exercicio6.py"
+"""
 
-varNum1 = float(input("Primeiro número: "))
-varNum2 = float(input("Segundo número: "))
-varNum3 = float(input("Terceiro número: "))
+# Recebe do usuário um número e armazena
+varNumero1 = float(input("Digite um número: "))
 
-varResultado = (varNum1 + varNum2 + varNum3) / 3
+# Recebe do usuário um número e armazena
+varNumero2 = float(input("Digite um número: "))
 
-print(f"A soma dos números é {varResultado:.2f}")
+# Recebe do usuário um número e armazena
+varNumero3 = float(input("Digite um número: "))
+
+# Calcula a média entre os números
+varMedia = (varNumero1 + varNumero2 + varNumero3) / 3
+
+# Exibe o resultado com duas casas decimais
+print(f'A média é {varMedia:.2f}.')
