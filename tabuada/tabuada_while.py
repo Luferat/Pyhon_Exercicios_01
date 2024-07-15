38fa90dadc9f91bc1dd33baed191beb39dc2d265
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-07-15 13:04:02 -0300

    Create tabuada_while.py

---
+++ tabuada/tabuada_while.py
@@ -0,0 +1,12 @@
+"""
+Tabuada de multiplicação usando `while` aninhados
+"""
+
+x = 0
+while x < 10:
+    y = 0
+    while y < 10:
+        print(x, 'x', y, '=', x*y)
+        y += 1
+    print('---------------------')
+    x += 1
