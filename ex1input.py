1e24b308cac184ee6103efd4308f2f23cec1a61c
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-07-15 09:34:46 -0300

    Update ex1input.py

--- ex1input.py
+++ ex1input.py
@@ -1,3 +1,13 @@
+"""
+Exemplo simples do uso de `input()` para receber dados do terminal.
+  Entrada padrão → Teclado
+"""
+
+# Solicita a informação no terminal usando a saída padrão.
 print('Digite seu nome e tecle [Enter]: ')
+
+# Recebe os dados do terminal usando a entrada padrão.
 varNome = input()
+
+# Exibe o resultado na saída padrão usando concatenação → +.
 print('Olá, ' + varNome)
