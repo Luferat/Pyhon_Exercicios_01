c3ce7598d0f964e33ced880063a797b86b4b77eb
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-07-15 10:00:50 -0300

    Update exercicio4.py

--- exercicio4.py
+++ exercicio4.py
@@ -1,23 +1,49 @@
+"""
+Exercício 4
+Escreva um programa que funcione como uma calculadora básica. 
+Ele deve pedir ao usuário dois números e uma operação ( + ,  - ,  * ,  / ).
+Com base na operação escolhida, o programa deve executar a operação e exibir o resultado.
+Salve como "exercicio4.py"
+"""
+
+# Pede atenção ao formato dos números
 print('Lembre-se de usar "." em vez de "," para as casas decimais dos números!!!\n')
 
-varNum1 = float(input("Primeiro número: "))
-varNum2 = float(input("Segundo número: "))
-varOperacao = input("Operação (+, -, *, /): ")
+# Recebe o primeiro valor, converte para inteiro e armazena na variável
+varNumero1 = int(input('Digite o primeiro número: '))
 
-if varOperacao == '+':
-    varResultado = varNum1 + varNum2
-    print(f"A soma entre '{varNum1}' e '{varNum2}' é '{varResultado}'")
-elif varOperacao == '-':
-    varResultado = varNum1 - varNum2
-    print(f"A subtração entre '{varNum1}' e '{varNum2}' é '{varResultado}'")
-elif varOperacao == '*':
-    varResultado = varNum1 * varNum2
-    print(f"A multiplicação entre '{varNum1}' e '{varNum2}' é '{varResultado}'")
-elif varOperacao == '/':
-    if varNum2 != 0:
-        varResultado = varNum1 / varNum2
-        print(f"A divisão de '{varNum1}' por '{varNum2}' e '{varResultado}'")
+# Recebe a operação como uma string e armazena na variável
+varOperador = str(input("Digite o operador (+, -, *, ou /): "))
+
+# Recebe o segundo valor, converte para inteiro e armazena na variável
+varNumero2 = int(input('Digite o segundo número: '))
+
+# Testa o operador '+'
+if varOperador == '+':
+    # Se for '+' realiza a soma e exibe
+    print(f'A soma entre {varNumero1} e {varNumero2} é {varNumero1 + varNumero2}!')
+
+# Testar o operador '-'
+elif varOperador == '-':
+    # Se for '-' realiza a subtração e exibe
+    print(f'A subtração entre {varNumero1} e {varNumero2} é {varNumero1 - varNumero2}!')
+
+# Testar o operador '*'
+elif varOperador == '*':
+    # Se for '*' realiza a multiplicação e exibe
+    print(f'A multiplicação entre {varNumero1} e {varNumero2} é {varNumero1 * varNumero2}!')
+
+# Testar o operador '/'
+elif varOperador == '/':
+    # Testa se o divisor (varNumero2) não é zero
+    if varNumero2 != 0:
+        # Se é diferente de 0, realiza a divisão e exibe com duas casas decimais
+        print(f'A divisão entre {varNumero1} e {varNumero2} é {varNumero1 / varNumero2:.2f}!')
     else:
-        print("Erro! Tentativa de divisão por zero.")
+        # Se digitou zero, exibe mensagem de erro
+        print('Oooops! Você está tentando dividir por ZERO.')    
+
+# Se a operação digitada não é suportada
 else:
-    print("Oooops! Operação inválida.")
+    # Exibe a operação como inválida!
+    print('Oooops! Não entendi o que você quer calcular!')    
