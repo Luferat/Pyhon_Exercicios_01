24be438042026dfdc01cb832eb338b3d471359ab
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-07-15 09:42:56 -0300

    Update ex1input.py

--- ex1input.py
+++ ex1input.py
@@ -1,12 +1,16 @@
 """
 Exemplo simples do uso de `input()` para receber dados do terminal.
-  Entrada padrão → Teclado
+
+Lembre-se!
+ 	┌─ Terminal ou console → Dispositivo de interface padrão do Python
+  ├──── Dispositivo de entrada padrão → Teclado → Função `input()`
+  └──── Dispositivo de saída padrão → Monitor → Função `print()`
 """
 
 # Solicita a informação no terminal usando a saída padrão.
 print('Digite seu nome e tecle [Enter]: ')
 
-# Recebe os dados do terminal usando a entrada padrão.
+# Recebe os dados do terminal usando a entrada padrão e armazena na variável.
 varNome = input()
 
 # Exibe o resultado na saída padrão usando concatenação → +.
