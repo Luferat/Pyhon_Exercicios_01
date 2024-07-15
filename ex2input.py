a51fe4759f32cf017c7d05f437dcaa48b58783b4
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-07-15 09:42:51 -0300

    Update ex2input.py

--- ex2input.py
+++ ex2input.py
@@ -1,6 +1,9 @@
 """
-O mesmo exemplo anterior, mas usando um formato otimizado e mais compacto.
+O mesmo exemplo anterior (`ex1input.py`), mas usando um formato otimizado e mais compacto.
 """
 
+# Recebe os dados do terminal usando a entrada padrão, exibindo a mensagem na saída padrão e armazena na variável.
 varNome = input("Digite seu nome e tecle [Enter]: ")
+
+# Exibe o resultado na saída padrão usando parâmetros.
 print("Seu nome é", varNome)
