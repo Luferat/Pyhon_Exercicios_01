4bf236ec7ddd2060f430fb19eedffe340bd5fefc
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-07-15 10:02:48 -0300

    Update exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -1,7 +1,21 @@
-print('Lembre-se de usar "." em vez de "," para as casas decimais dos números!!!\n')
+"""
+Exercício 5
+Escreva um programa que peça ao usuário três informações:
+ - um número inteiro
+ - um número decimal
+ - uma palavra
+O programa deve converter essas entradas para os tipos corretos e exibir as informações formatadas em uma frase.
+Salve como "exercicio5.py"
+"""
 
-varInteger = int(input("Digite um número inteiro: "))
-varFloat = float(input("Digite um número decimal: "))
-varString = str(input("Digite uma palavra: "))
+# Solicita ao usuário um número inteiro e armazena em uma variável
+varInteiro = int(input("Digite um número inteiro: "))
 
-print(f"Você digitou o inteiro '{varInteger}', o flutuante '{varFloat}' e a palavra '{varString}'.")
+# Solicita ao usuário um número decimal e armazena em uma variável
+varDecimal = float(input("Digite um número decimal usando '.': "))
+
+# Solicita ao usuário uma palavra e armazena em uma variável
+varPalavra = str(input("Digite uma palavra: "))
+
+# Exibe as informações digitadas formatadas
+print(f'Você digitou o inteiro "{varInteiro}", o decimal "{varDecimal}" e a palavra "{varPalavra}".')
