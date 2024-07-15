e702500f1df6e86244045de529f850f3ac804f5d
André Luferat <33375030+Luferat@users.noreply.github.com>
2024-07-15 09:47:55 -0300

    Update exercicio1.py

--- exercicio1.py
+++ exercicio1.py
@@ -1,6 +1,26 @@
-varIdade = int(input("Digite sua idade: "))
+"""
+Exercício 1
+Escreva um programa Python que peça ao usuário a sua idade e verifique se ele é elegível para votar. 
+No Brasil, a idade mínima para votar é 16 anos. 
+Salve como "exercicio1.py".
+"""
 
-if varIdade >= 16:
-    print("Oba! Você pode votar!")
+# Receber a idade do usuário como inteiro em uma variável
+var_idade = int(input('Digite sua idade: '))
+
+# Testa se a idade é maior ou igual a 16 anos
+if var_idade >= 16:
+    # Se é maior ou igual (verdade → true)
+    # Mostre a mensagem abaixo no terminal
+    print('Oba! você já pode votar...')
 else:
-    print("Opa! Você ainda não pode votar!")
+    # Se é menor (mentira → false)
+    # Mostre a mensagem abaixo no terminal
+    print('Oooops! Você ainda não pode votar...')
+
+"""
+Teste assim!
+Versão super compacta.
+"""
+# if int(input("Digite sua idade: ")) >= 16: print("Oba! Você pode votar!")
+# else: print("Opa! Você ainda não pode votar!")
